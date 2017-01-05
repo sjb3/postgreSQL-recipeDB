e7b873de243cc6ca50275f20c30d7e7cd182ac9f
Sung J Byun <sungjbyun@D-108-179-144-63.dhcp4.washington.edu>
2017-01-04 18:34:55 -0800

    playing w postgreSQL

--- app.js
+++ app.js
@@ -27,10 +27,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
-  res.render('index');
+  // PG connect
+  pg.connect(connect, function(err, client, done){
+    if(err){
+      return console.error('error fetching client from pool', err)
+    }
+    //execute a query on our db
+    client.query('SELECT * FROM recipe', function(err, result){
+      if(err) throw err;
+      //just print the result to the console
+      res.render('index', {recipes: result.rows});
+      done();
+      //disconnect the client
+    })
+  })
 });
 
 //Server
 app.listen(3000, function(){
   console.log('Server UP <8))><~ on: port 3000');
-})
+});
